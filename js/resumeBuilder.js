85e7cf70d49a0c00d17479f68de7fc712c712b07
josue <josue@example.com>
2015-03-08 20:24:46 +0100

    some refactoring

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,4 @@
 
-/*
-var bio = {
-	"name":firstName,
-	"role":role,
-	"contactInfo": ["cochusco@gmail.com","23433333","twitter"],
-	"pictureURL": "images/fry.jpg" ,
-	"welcomeMessage": "Welcome to my page",
-	"skills":["java" , "wen developer" , "a bit android and ios"] 
-
-};*/
-
 var bio = {
 	"name":"Josue Martinez",
 	"role":"Web Developer",
@@ -22,10 +11,9 @@ var bio = {
 	},
 	"welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc.",
 	"skills" : ["awesomenes" , "delivering things"  ,"cryogenic sleep","saving the universe"],
-	"bioPic": "images/fry.jpg"
+	"bioPic": "images/fry.jpg",
+	"display" : null
 } ;
-
-
 
 var education = {
 		"schools" : [
@@ -41,26 +29,25 @@ var education = {
 				"title":"aplicaciones seguras" ,
 				"school":"autonomo" ,
 				"date": 2005 ,
-				"url": "aplicacionesseguras.com" 
+				"url": "http://aplicacionesseguras.com" 
 			},
 			{
 				"title":"modelado SQL" ,
 				"school":"IE" ,
 				"date": 2007 ,
-				"url": "slqcourses.com" 
+				"url": "http://slqcourses.com" 
 			},
 			{
 				"title":"UML" ,
 				"school":"IE" ,
 				"date": 2010 ,
-				"url": "learnunml.org" 
-			}
+				"url": "http://learnunml.org"
+						}
 
-		]
+		],
+		"display" : null
 		
 	} ;
-
-
 
 var work = {
 	"jobs" : [
@@ -83,28 +70,113 @@ var work = {
 
 } ;
 
-var projects = [
+var projects = {
+	"projects":[
 	{
 		"title": "The internet box" ,
 		"dates": "2000" ,
 		"description": "Queso babybel camembert de normandie danish fontina halloumi. Brie cheese and biscuits say cheese. Blue castello chalk and cheese stinking bishop camembert de normandie bavarian bergkase goat feta cheese triangles. Cut the cheese melted cheese cheesy grin cheese on toast squirty cheese hard cheese macaroni cheese gouda. Melted cheese cheese and biscuits lancashire taleggio the big cheese." ,
-	    "images": ["http://d4c027c89b30561298bd-484902fe60e1615dc83faa972a248000.r12.cf3.rackcdn.com/imagepicker/21974/internet-box-it-crowd_1.jpg" , "http://www.kissmybit.com/wp-content/uploads/2012/03/The-IT-Crowd-the-it-crowd-20584011-760-505.jpg"] 
+	    "images": ["images/box.jpg" , "images/it.jpg"] 
 	},
 	{
 		"title": "Sample ployect " ,
 		"dates": "2000-2005" ,
 		"description": "Rubber cheese airedale halloumi. Fromage boursin roquefort the big cheese paneer parmesan the big cheese ricotta. Cottage cheese taleggio airedale cheese slices cheeseburger squirty cheese bocconcini lancashire. Ricotta babybel who moved my cheese cheesy feet croque monsieur." ,
-	    "images": ["http://prancercise.com/wp-content/uploads/2013/07/book-larger.jpg"] 
+	    "images": ["images/pran.jpg"] 
 
 	}
-];
+	],
+	"display" : null	
+}  ;
 
 
 
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// TODO look at the # hrefs to substitute
 // capitalice firt letter of the name an all surname
+
+
+
+
+
+bio.display = function(){
+	$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+	$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+	var contactArr =[HTMLmobile.replace("%data%", bio.contacts.mobile) , HTMLemail.replace("%data%",bio.contacts.email) ,
+				HTMLtwitter.replace("%data%",bio.contacts.twitter) , HTMLgithub.replace("%data%",bio.contacts.github) ,
+				HTMLlocation.replace("%data%",bio.contacts.location)];
+	$("#topContacts").append(contactArr);
+	$("#footerContacts").append(contactArr);
+	$("#header").append([HTMLbioPic.replace("%data%", bio.bioPic),HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage)]);
+	if(bio.skills.length > 0){
+		$("#header").append(HTMLskillsStart);
+		var allFormattedHTMLskills ="";
+		for (var i in bio.skills){
+			allFormattedHTMLskills = allFormattedHTMLskills + HTMLskills.replace("%data%", bio.skills[i]);
+		}
+		$("#skills").append(allFormattedHTMLskills);
+		$("#skills").css("flex-direction","column") // I had to do this because the defaul layout is row , and so is showed
+	}
+} ;
+
+
+
+function displayWork(){
+	$("#workExperience").append(HTMLworkStart);
+	for (var job in work.jobs) {
+		var workItem =  HTMLworkEmployer.replace( "%data%" ,  work.jobs[job].employer  ) + HTMLworkTitle.replace("%data%" , work.jobs[job].title) + HTMLworkDates.replace( "%data%" , work.jobs[job].dates ) + HTMLworkLocation.replace( "%data%" , work.jobs[job].location ) + HTMLworkDescription.replace( "%data%" , work.jobs[job].description ) ;		
+	 	$(".work-entry:last").append(workItem); 	
+	} ;
+} ;
+
+
+
+
+
+projects.display = function(){	
+		for (var project in projects.projects) {
+			//if(typeof(projects[project]) != "object") continue ; //do not insert project in case of a function element (ie, this function)
+			// console.log("display" + project);
+			$("#projects").append(HTMLprojectStart);
+			var projectItem =  HTMLprojectTitle.replace( "%data%" ,  projects.projects[project].title  ) + HTMLprojectDates.replace("%data%" , projects.projects[project].dates) + HTMLprojectDescription.replace( "%data%" , projects.projects[project].description )    ;		
+			for (var image in projects.projects[project].images){			
+					projectItem =  projectItem + HTMLprojectImage.replace( "%data%" , projects.projects[project].images[image]) ;
+			}
+		 	$(".project-entry:last").append(projectItem); 	
+	} ;
+};
+
+
+
+education.display = function(){			
+		for(var school in education.schools){
+			$("#education").append(HTMLschoolStart);
+			var educationEntry = HTMLschoolName.replace("#", education.schools[school].url).replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree) + HTMLschoolDates.replace("%data%", education.schools[school].dates) + HTMLschoolLocation.replace("%data%", education.schools[school].location) ;
+			for(var major in education.schools[school].majors){
+				educationEntry += HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]) ;
+			}					 
+ 			$(".education-entry:last").append(educationEntry) ;
+		} ;
+		$("#education").append("<h3>Online Classes</h3>");
+		for(var onlineCourse in education.onlineCourses){		
+			$("#education").append(HTMLschoolStart);	
+			var educationOnlineEntry = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school) 
+										+ HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].date) + HTMLonlineURL.replace("#", education.onlineCourses[onlineCourse].url) .replace("%data%", education.onlineCourses[onlineCourse].url)  ;
+			/// HTMLonlineTitle.replace("#", education.onlineCourses[onlineCourse].url).replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree) + HTMLschoolDates.replace("%data%", education.schools[school].dates) + HTMLschoolLocation.replace("%data%", education.schools[school].location) ;
+								 
+ 			$(".education-entry:last").append(educationOnlineEntry);
+
+		} ;		
+};
+
+
+function locationizer(workObjt){
+	var locations = [];
+	for(var job in workObjt.jobs){
+		locations.push(workObjt.jobs[job].location);
+	}
+	return locations ;
+};
+
 function inName(){
 	var name = bio.name ;
 	console.log("inNme"  +  name);
@@ -117,125 +189,23 @@ function inName(){
 		nameCpy.push(words[i].toUpperCase());
 	}
 	return nameCpy.join(" ") ;
-}
-
-$("#main").append(internationalizeButton);
-// var internationalizeButton = '<button>Internationalize</button>';
+} ;
 
 
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-
-// var formattedContactGeneric = HTMLcontactGeneric.replace("%data%",);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
-var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
-var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
-// var formattedblog = HTMLblog.replace("%data%",);
-var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
-var contactArr =[formattedMobile , formattedEmail ,formattedTwitter , formattedGithub ,formattedLocation];
-
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-
-
-
-$("#topContacts").append(contactArr);
-$("#footerContacts").append(contactArr);
-
-$("#header").append([formattedBioPic,formattedWelcomeMsg]);
-if(bio.skills.length > 0){
-	$("#header").append(HTMLskillsStart);
-	var allFormattedHTMLskills ="";
-	for (var i in bio.skills){
-		allFormattedHTMLskills = allFormattedHTMLskills + HTMLskills.replace("%data%", bio.skills[i]);
-	}
-	$("#skills").append(allFormattedHTMLskills);
-	$("#skills").css("flex-direction","column") // I had to do this because the defaul layout is row , and so is showed
-}
- 
-$(document).click(function(loc) {
-  	console.log(loc.target);
-  	console.log(loc.relatedTarget);
-  	console.log(loc.pageX);
-  	console.log(loc.pageY);
-  	console.log(loc.which);
-  	console.log(loc.metaKey);  	
-});
-
-function displayWork(){
-	$("#workExperience").append(HTMLworkStart);
-	for (var job in work.jobs) {
-		var workItem =  HTMLworkEmployer.replace( "%data%" ,  work.jobs[job].employer  ) + HTMLworkTitle.replace("%data%" , work.jobs[job].title) + HTMLworkDates.replace( "%data%" , work.jobs[job].dates ) + HTMLworkLocation.replace( "%data%" , work.jobs[job].location ) + HTMLworkDescription.replace( "%data%" , work.jobs[job].description ) ;		
-	 	$(".work-entry:last").append(workItem); 	
-	} ;
-}
-
+bio.display();
 work.display = displayWork;
 work.display() ;
-
-
-
-projects.display = function(){	
-		for (var project in projects) {
-			if(typeof(projects[project]) != "object") continue ; //do not insert project in case of a function element (ie, this function)
-			console.log("display" + project);
-			$("#projects").append(HTMLprojectStart);
-			var projectItem =  HTMLprojectTitle.replace( "%data%" ,  projects[project].title  ) + HTMLprojectDates.replace("%data%" , projects[project].dates) + HTMLprojectDescription.replace( "%data%" , projects[project].description )    ;		
-			for (var image in projects[project].images){			
-					projectItem =  projectItem + HTMLprojectImage.replace( "%data%" , projects[project].images[image]) ;
-			}
-		 	$(".project-entry:last").append(projectItem); 	
-	} ;
-};
-
 projects.display() ;
-
-
-education.display = function(){			
-		for(var school in education.schools){
-			$("#education").append(HTMLschoolStart);
-			var educationEntry = HTMLschoolName.replace("#", education.schools[school].url).replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree) + HTMLschoolDates.replace("%data%", education.schools[school].dates) + HTMLschoolLocation.replace("%data%", education.schools[school].location) ;
-			for(var major in education.schools[school].majors){
-				educationEntry += HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]) ;
-			}					 
- 			$(".education-entry:last").append(educationEntry) ;
-		} ;
-
-		$("#education").append("<h3>Online Classes</h3>");
-
-		for(var onlineCourse in education.onlineCourses){		
-			$("#education").append(HTMLschoolStart);	
-			var educationOnlineEntry = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school) 
-										+ HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].date) + HTMLonlineURL.replace("#", education.onlineCourses[onlineCourse].url) .replace("%data%", education.onlineCourses[onlineCourse].url)  ;
-			/// HTMLonlineTitle.replace("#", education.onlineCourses[onlineCourse].url).replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree) + HTMLschoolDates.replace("%data%", education.schools[school].dates) + HTMLschoolLocation.replace("%data%", education.schools[school].location) ;
-								 
- 			$(".education-entry:last").append(educationOnlineEntry);
-
-		} ;
-
-
-		
-};
-
 education.display() ;
-
-
-
-// TODO look at the # hrefs to substitute
-
-
-
-
-
-function locationizer(workObjt){
-	var locations = [];
-	for(var job in workObjt.jobs){
-		locations.push(workObjt.jobs[job].location);
-	}
-	return locations ;
-};
-
+$("#main").append(internationalizeButton);
 $("#mapDiv").append(googleMap);
+
+
+
+/* Not necesary
+$(document).click(function(loc) {  	
+  	console.log(loc.pageX);
+  	console.log(loc.pageY);  	
+}); */
+
